4e32e0e16ea63f43e85b3903faa2ef86f2a77563
alabhastam <abdollahia713@gmail.com>
2024-09-06 07:56:07 -0700

    LOCK

---
+++ LevelManagers/LockUIPosition.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class LockUIPosition : MonoBehaviour
+{
+    public RectTransform lifeTextRect; 
+    public RectTransform coinTextRect; 
+    public Vector2 lifeTextAnchorPosition; 
+    public Vector2 coinTextAnchorPosition; 
+
+    void Start()
+    {
+        lifeTextRect.anchoredPosition = lifeTextAnchorPosition;
+
+        coinTextRect.anchoredPosition = coinTextAnchorPosition;
+    }
+}
