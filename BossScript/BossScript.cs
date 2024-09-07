1f2c53d310c2c3767459cd4e90b4185e23d8b450
alabhastam <abdollahia713@gmail.com>
2024-09-07 04:57:51 -0700

    Upgrade-Boss

--- BossScript/BossScript.cs
+++ BossScript/BossScript.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 
 public class BossScript : MonoBehaviour
 {
-
     public GameObject stone; 
     public Transform attackInstantiate; 
-    private Animator anim ; 
+    private Animator anim; 
     private string coroutine_name = "StartAttack"; 
+    private Transform player; 
+    public float attackForce = 600f; 
+    public float verticalForceMultiplier = 0.25f; 
+
     private void Awake() {
         anim = GetComponent<Animator>(); 
     }
+
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         StartCoroutine(coroutine_name);
     }
 
     void Attack(){
-        GameObject obj = Instantiate(stone, attackInstantiate.position,Quaternion.identity );   
-        obj.GetComponent<Rigidbody2D>().AddForce(new Vector2 (Random.Range(-300f,-700f),0f));
+        GameObject obj = Instantiate(stone, attackInstantiate.position, Quaternion.identity); 
+
+        Vector2 direction = (player.position - attackInstantiate.position).normalized;
+
+        direction.y += verticalForceMultiplier;
+
+        obj.GetComponent<Rigidbody2D>().AddForce(direction * attackForce);
     }
 
     void BackToIdle(){
@@ -31,11 +41,11 @@ public class BossScript : MonoBehaviour
         StopCoroutine(coroutine_name);
         enabled = false;
     }
+
     // Update is called once per frame
     IEnumerator StartAttack (){
         yield return new WaitForSeconds(Random.Range(2f,5f));
         anim.Play("BossAttack"); 
- 
         StartCoroutine(coroutine_name);
     }
 }
